0e44139a506279fac6e7b3862274e9a2028cf5b5
EsterStar <esterguimaraesfsg@gmail.com>
2025-05-11 20:17:37 -0300

    Create Codigo Fatorial.c

---
+++ Codigo Fatorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int fat = 1;
+    int i;
+
+    printf("Digite o valor de n: );
+    scanf("%d, &n);
+
+    for(i = n; i > 0; i--){
+        fat = fat *i;
+        printf("%d %d\n, i, fat);
+    }
+
+    return 0;
+}
