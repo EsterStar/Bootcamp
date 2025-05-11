fb81202d55e993bdc9eb3fd5b2c06881ab3fe650
EsterStar <esterguimaraesfsg@gmail.com>
2025-05-11 20:21:10 -0300

    Create Codigo X.c

---
+++ Codigo X.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    printf("0          0\n");
+    printf(" 0        0\n");
+    printf("  0      0\n");
+    printf("   0    0\n");
+    printf("    0  0\n");
+    printf("     00\n");
+    printf("    0  0\n");
+    printf("   0    0\n");
+    printf("  0      0\n");
+    printf(" 0        0\n");
+    printf("0          0\n");
+
+    
+
+
+    return 0;
+}
